8ac549716a2605d5e1fdd6524f9136a66e7a558f
YEOP <64470512+geonyeop123@users.noreply.github.com>
2022-11-13 22:28:56 +0900

    switch 문 리팩터링
    amountFor 함수로 추출

--- Chapter1/performanceFee.js
+++ Chapter1/performanceFee.js
@@ -36,38 +36,44 @@ function statement(invoice, plays) {
 
   for (let perf of invoice.performances) {
     const play = plays[perf.payID];
-    let thisAmont = 0;
+    let thisAmount = amountFor(perf, play);
 
-    switch (play.type) {
-      case "tragedy": // 비극
-        thisAmont = 40000;
-        if (perf.audience > 30) {
-          thisAmont += 1000 * (perf.audience - 30);
-        }
-        break;
-      case "comedy": // 희극
-        thisAmont = 30000;
-        if (perf.audience > 20) {
-          thisAmont += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmont += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르 : ${play.type}`);
-    }
     // 포인트를 적립한다.
     volumeCredits += Math.max(pref.audience - 30, 0);
     // 희극 관객 5명마다 추가 포인트를 제공한다.
     if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
 
     // 청구 내용을 출력한다.
-    result += ` ${play.name} : ${format(thisAmont / 100)} (${
+    result += ` ${play.name} : ${format(thisAmount / 100)} (${
       perf.audience
     }석)\n`;
-    totalAmount += thisAmont;
+    totalAmount += thisAmount;
   }
 
   result += `총액 : ${format(totalAmount / 100)}\n`;
   result += `적립 포인트 : ${volumeCredits}점\n`;
   return result;
+
+  function amountFor(perf, play) {
+    let thisAmount = 0;
+    switch (play.type) {
+      case "tragedy": // 비극
+        thisAmount = 40000;
+        if (perf.audience > 30) {
+          thisAmount += 1000 * (perf.audience - 30);
+        }
+        break;
+      case "comedy": // 희극
+        thisAmount = 30000;
+        if (perf.audience > 20) {
+          thisAmount += 10000 + 500 * (perf.audience - 20);
+        }
+        thisAmount += 300 * perf.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르 : ${play.type}`);
+    }
+
+    return thisAmount;
+  }
 }
