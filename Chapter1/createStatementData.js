e17c598a2f974bd7a2ae5cf1a68208867a4fce53
YEOP <64470512+geonyeop123@users.noreply.github.com>
2022-11-14 18:37:40 +0900

    파일 쪼개기 및 html 출력 함수 생성

--- Chapter1/createStatementData.js
+++ Chapter1/createStatementData.js
@@ -24,13 +24,13 @@ let invoices = [
   },
 ];
 
-function statement(invoice, plays) {
-  const statementData = {};
-  statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances.map(enrichPerformance);
-  statementData.totalAmount = totalAmount(statementData);
-  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-  return renderPlainText(statementData, plays);
+export default function createStatementData(invoice, plays) {
+  const result = {};
+  result.customer = invoice.customer;
+  result.performances = invoice.performances.map(enrichPerformance);
+  result.totalAmount = totalAmount(result);
+  result.totalVolumeCredits = totalVolumeCredits(result);
+  return result;
 
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance);
@@ -80,23 +80,5 @@ function statement(invoice, plays) {
 
   function totalVolumeCredits(data) {
     return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
-  }
-}
-
-function renderPlainText(data, plays) {
-  let result = `청구 내역 (고객명 : ${data.customer})\n`;
-  for (let perf of data.performances) {
-    result += ` ${data.play.name} : ${usd(perf.amount)} (${perf.audience}석)\n`;
-  }
-  result += `총액 : ${format(data.totalAmount / 100)}\n`;
-  result += `적립 포인트 : ${data.totalVolumeCredits}점\n`;
-  return result;
-
-  function usd(aNumber) {
-    return new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 2,
-    }).format(aNumber / 100);
   }
 }
