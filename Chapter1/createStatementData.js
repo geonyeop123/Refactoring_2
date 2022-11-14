49c5b0952f0aa3e46b125c88963e46dfac4ed321
YEOP <64470512+geonyeop123@users.noreply.github.com>
2022-11-14 19:05:30 +0900

    공연료 계산기 생성

--- Chapter1/createStatementData.js
+++ Chapter1/createStatementData.js
@@ -24,6 +24,13 @@ let invoices = [
   },
 ];
 
+class PerformanceCalculator {
+  constructor(aPerformance, aPlay) {
+    this.performance = aPerformance;
+    this.play = aPlay;
+  }
+}
+
 export default function createStatementData(invoice, plays) {
   const result = {};
   result.customer = invoice.customer;
@@ -33,8 +40,12 @@ export default function createStatementData(invoice, plays) {
   return result;
 
   function enrichPerformance(aPerformance) {
+    const calculator = new PerformanceCalculator(
+      aPerformance,
+      playFor(aPerformance)
+    );
     const result = Object.assign({}, aPerformance);
-    result.play = playFor(result);
+    result.play = calculator.play;
     result.amount = amountFor(result);
     result.volumeCredits = volumeCreditsFor(result);
     return result;
