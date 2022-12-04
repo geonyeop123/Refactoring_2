960034d7829695a5b848cb139f449526f74186c3
YEOP <64470512+geonyeop123@users.noreply.github.com>
2022-12-04 11:30:40 +0900

    chapter 4

---
+++ Chapter4/index.js
@@ -0,0 +1,86 @@
+function sampleProvinceData() {
+  return {
+    name: "Asia",
+    producers: [
+      { name: "Byzantium", cost: 10, production: 9 },
+      { name: "Attalia", cost: 12, production: 10 },
+      { name: "Sinope", cost: 10, production: 6 },
+    ],
+    demand: 30,
+    price: 20,
+  };
+}
+
+class Province {
+  constructor(doc) {
+    this.name = doc.name;
+    this.producers = [];
+    this.totalProduction = 0;
+    this.demand = doc.demand;
+    this.price = doc.price;
+    doc.producers.forEach((d) => this.addProducer(new Producer(tyhis, d)));
+  }
+
+  addProducer(arg) {
+    this.producers.push(arg);
+    this.totalProduction += arg.production;
+  }
+
+  get shortfall() {
+    return this.demand - this.totalProduction;
+  }
+
+  get name() {
+    return this.name;
+  }
+  get producers() {
+    return this.producers;
+  }
+  get totalProduction() {
+    return this.totalProduction;
+  }
+  set totalProduction(arg) {
+    return (this.totalProduction = arg);
+  }
+  get demand() {
+    return this.demand;
+  }
+  set demand(arg) {
+    this.demand = parseInt(arg);
+  } // 숫자로 파싱해서 저장
+  get price() {
+    return this.price;
+  }
+  set price(arg) {
+    this.price = parseInt(arg);
+  } // 숫자로 파싱해서 저장
+}
+
+class Producer {
+  constructor(aProvince, data) {
+    this.province = aProvince;
+    this.cost = data.cost;
+    this.name = data.name;
+    this.production = data.production || 0;
+  }
+
+  get name() {
+    return this.name;
+  }
+  get cost() {
+    return this.cost;
+  }
+  set cost(arg) {
+    this.cost = parseInt(arg);
+  }
+
+  get production() {
+    return this.production;
+  }
+  set production(amountStr) {
+    const amount = parseInt(amountStr);
+    const newProduction = Number.isNaN(amount) ? 0 : amount;
+    this.province.totalProduction += newProduction - this.production;
+    this.production = newProduction;
+  }
+}
